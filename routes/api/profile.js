8a1be1c813965c2f5d014cd6a1495c1f15d7dee7
monica6423 <monica6423@gmail.com>
2020-08-22 13:59:49 +0800

    get all profiles and profile bu user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -101,3 +101,40 @@ router.post('/', [ auth, [
 
     res.send('hih');
 });
+
+//@route GET api/profile
+//@decs Get all profile
+//@access Public
+router.get('/', async (req, res) => {
+    try {
+        //populate from user collection and an array of fields
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+       
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route GET api/profile/user/:user_id
+//@decs Get profile by user id
+//@access Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        //
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile) return res.status(400).json( {msg: 'There is no profile for this user'});
+        res.json(profile);
+    } catch (err) {
+        console.error(err);
+        
+        if(err.name == 'CastError') {
+            
+            return res.status(400).json({ msg: 'Profile not found'});
+            
+        }
+        res.status(500).send('Server Error');
+    }
+});
