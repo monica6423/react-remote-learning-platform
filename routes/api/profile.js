bd1bf06554d1a758b04288b3f4f1ffcd3178420d
monica6423 <monica6423@gmail.com>
2020-08-22 14:01:34 +0800

    delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -138,3 +138,21 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//@route DELETE api/profile
+//@decs Delete profile, user and post
+//@access Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //remove users posts
+        await Post.deleteMany({ user: req.user.id });
+        //remove profile
+        await Profile.findOneAndRemove({ user: req.user.id});
+        //remove user
+        await User.findOneAndRemove({ _id: req.user.id});
+        res.json({msg: 'User remove'});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
