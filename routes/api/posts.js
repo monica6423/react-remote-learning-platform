7639fa02cfe707258781b6038ceda695e40aa833
monica6423 <monica6423@gmail.com>
2020-08-23 10:00:01 +0800

    post likeand unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,4 +38,63 @@ router.post('/', [ auth, [
 
     
 });
+//@route GET api/posts
+//@decs Get all posts
+//@access Private
+
+router.get('/', auth, async ( req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1});
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      // Check for ObjectId format and post
+      if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post) {
+        return res.status(404).json({ msg: 'Post not found1' });
+      }
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      if(err.name == 'CastError') {
+        return res.status(400).json({ msg: 'Post not found2' });
+        }
+      res.status(500).send('Server Error');
+    }
+  });
+
+//@route Delete api/posts/:id
+//@decs Delete a posts
+//@access Private
+
+router.delete('/:id', auth, async ( req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) {
+            return res.status(400).json({ msg: 'Post not found2' });
+            }
+        //check user
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({ msg:'User not authorized'});
+        }
+
+        await post.remove();
+        res.json({ msg: 'post removed'});
+    } catch (err) {
+        console.error(err.message);
+        if(err.name == 'CastError') {
+            return res.status(400).json({ msg: 'Post not found2' });
+            }
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
