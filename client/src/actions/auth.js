da66616beffdc21dc0726fe9a8a6245b7cada267
monica6423 <monica6423@gmail.com>
2020-08-25 23:21:32 +0800

    logout and navbar

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -99,3 +99,8 @@ export const login = ( email, password) => async dispatch => {
     }
 };
 
+//Logout /Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE});
+    dispatch({ type: LOGOUT});
+};
