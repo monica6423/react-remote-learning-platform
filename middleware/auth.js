91c9afd3e9406e2a4075ac2a3ae9b4442d3c8cf3
monica chang <monica6423@gmail.com>
2022-09-20 11:13:04 +0300

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,10 +12,7 @@ module.exports = function (req, res, next) {
 
     //verify token
     try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-        req.user = decoded.user;
-        
+        jwt.verify(token, config.get('jwtSecret'));        
         next();
     } catch (err) {
         res.status(401).json({ msg: 'token is not valid' });
