dca816a9ebf2ffce63f0c667d628798e5ecb26f1
monica6423 <monica6423@gmail.com>
2024-06-04 15:45:06 +0300

    Update URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.NODE_ENV === "production"
-? "https://talku-talku-v3-server.vercel.app"
+? "https://react-remote-learning-platform-backend.vercel.app"
 : "http://localhost:5050";
 
 export const API = axios.create({
