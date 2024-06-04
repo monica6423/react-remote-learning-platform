8fc1a37c2e0d301772482016e33e93a927fa8bb2
monica6423 <monica6423@gmail.com>
2024-06-04 13:09:14 +0300

    Minor change

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -21,7 +21,7 @@ export const loadUser = () => async dispatch => {
     }
     try {
         const res = await axios.get('/api/auth');
-    
+        console.log("res.data", res.data)
         dispatch({
             type:USER_LOADED,
             payload:res.data
